554ad2a7d22e62bb69863d2a345281d49b49a948
iocaine <lonermine@gmail.com>
2020-05-05 22:13:39 +0300

    Второе задание

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 let budget = prompt('What is your budget?'); // Спрашиваем бюджет магазина
 let shopName = prompt('What is your shop name?'); // Спрашиваем название магазина
-let shopGoods1 = prompt('What type of goods do you sell?'); // Узнаем первый товар
-let shopGoods2 = prompt('What type of goods do you sell?'); // Узнаем второй товар
-let shopGoods3 = prompt('What type of goods do you sell?'); // Узнаем третий товар
-
-let dailyBudget = budget/30; // Высчитываем бюджет на день
-
-alert("Ваш бюджет в день:" + dailyBudget); // Выводим бюджет на день
+let time = 19;
 
 let mainList = { // всю инфу заносим в объект
 	budget: budget,
 	shopName: shopName,	
-	open: true,
-	shopGoods: [shopGoods1,shopGoods2,shopGoods3],
+	open: false,
+	shopGoods: [],
 	employers: {
 
 	}
 };
+
+/*for (let i = 0; i < 5; i++) {
+	let a = prompt('What type of goods do you sell?');
+	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+		console.log('All is correct!');
+		mainList.shopGoods[i] = a;
+	} else {
+		i = i - 1;
+	}
+}*/
+
+let i = 0;
+while (i < 5) {	
+	let a = prompt('What type of goods do you sell?');
+	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+	console.log("All is correct!");
+	i++;
+	} else {
+		i = i--;
+	}
+}
+
+if(time < 0) {
+	console.log('It is impossible!');
+} else if(time > 8 && time < 20) {
+	console.log('It is time to work!');
+	} else if(time < 24) {
+	console.log('It is too late!');
+		} else { 
+			console.log('There is only 24 hours in a day!');
+};
+
+alert(mainList.budget / 30); // Выводим бюджет на день
+
+console.log(mainList);
 
 function digit (number) {
   var figures = "" + number
