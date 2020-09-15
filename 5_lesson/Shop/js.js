2a9430c1fdbcc61118b8b5bc95efb319680786c0
iocaine <lonermine@gmail.com>
2020-09-15 13:39:00 +0300

    version_2

--- 5_lesson/Shop/js.js
+++
@@ -1,7 +0,0 @@
-let buttonOpen = document.getElementsByClassName('open-btn'),
-	mainInfo = document.getElementsByClassName('main-info')[0].children,
-	buttons = [document.getElementsByTagName('button')[1],document.getElementsByTagName('button')[2],document.getElementsByTagName('button')[3]],
-	chooseItem = document.querySelectorAll('.choose-item'),
-	timeValue = document.querySelectorAll('.time-value'),
-	countBudget = document.querySelectorAll('.count-budget-value'),
-	employersItem = document.querySelectorAll('.hire-employers-item')
